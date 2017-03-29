4611852d152c33cc7566bddf5807b6e3bbf8917a
Giovanna Calderoni <g.c1@me.com>
2017-03-29 18:44:33 -0500

    Updates to firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebase = require("firebase");
 // The following initializes Firebase:
     // TODO: Replace code snippet
 const config = {
-  apiKey: "<API_KEY>",
+  apiKey: "<c6b1a175a6d44537a7045b2057893d77>",
   authDomain: "<PROJECT_ID>.firebaseapp.com",
   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
   storageBucket: "<BUCKET>.appspot.com",
